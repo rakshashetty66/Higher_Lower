7fb91da2090ff956e95766d8731b9b95096de536
Raksha Shetty <126263491+rakshashetty66@users.noreply.github.com>
2024-10-11 13:30:47 +0530

    higher_lower_game.py

---
+++ higher_lower_game.py
@@ -0,0 +1,320 @@
+data = [
+    {
+        "name": "Jenny's Lectures",
+        "follower_count": 1,
+        "description": "Youtuber",
+        "country": "India"
+    },
+    {
+        "name": "Cristiano ronaldo",
+        "follower_count": 600,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Fit tuber",
+        "follower_count": 7,
+        "description": "Youtuber",
+        "country": "India"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 420,
+        "description": "Musician and Actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 400,
+        "description": "Reality TV Personality and Businessman",
+        "country": "United States"
+    },
+    {
+        "name": "Pankaj Tripathi",
+        "follower_count": 5,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 480,
+        "description": "Footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 260,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 350,
+        "description": "Actor and Wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 320,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Shah Rukh Khan",
+        "follower_count": 35,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 250,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 310,
+        "description": "Musician and Actress",
+        "country": "United States"
+    },
+    {
+        "name": "Billie Eilish",
+        "follower_count": 120,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Robert Downey Jr.",
+        "follower_count": 90,
+        "description": "Actor",
+        "country": "United States"
+    },
+    {
+        "name": "LeBron James",
+        "follower_count": 150,
+        "description": "Basketball Player",
+        "country": "United States"
+    },
+    {
+        "name": "Emma Watson",
+        "follower_count": 80,
+        "description": "Actress",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 110,
+        "description": "Actress and Singer",
+        "country": "United States"
+    },
+    {
+        "name": "Chris Hemsworth",
+        "follower_count": 110,
+        "description": "Actor",
+        "country": "Australia"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 250,
+        "description": "Musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "follower_count": 90,
+        "description": "Actress and Producer",
+        "country": "India"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 340,
+        "description": "Reality TV Personality",
+        "country": "United States"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 200,
+        "description": "Model",
+        "country": "United States"
+    },
+    {
+        "name": "Will Smith",
+        "follower_count": 100,
+        "description": "Actor",
+        "country": "United States"
+    },
+    {
+        "name": "Rihanna",
+        "follower_count": 280,
+        "description": "Musician and Entrepreneur",
+        "country": "Barbados"
+    },
+    {
+        "name": "Drake",
+        "follower_count": 240,
+        "description": "Musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Tom Holland",
+        "follower_count": 85,
+        "description": "Actor",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Gal Gadot",
+        "follower_count": 75,
+        "description": "Actress",
+        "country": "Israel"
+    },
+    {
+        "name": "Zlatan Ibrahimović",
+        "follower_count": 50,
+        "description": "Footballer",
+        "country": "Sweden"
+    },
+    {
+        "name": "Deepika Padukone",
+        "follower_count": 70,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Ryan Reynolds",
+        "follower_count": 80,
+        "description": "Actor",
+        "country": "Canada"
+    },
+    {
+        "name": "Kevin Hart",
+        "follower_count": 120,
+        "description": "Comedian and Actor",
+        "country": "United States"
+    },
+    {
+        "name": "Jason Momoa",
+        "follower_count": 50,
+        "description": "Actor",
+        "country": "United States"
+    },
+    {
+        "name": "Ed Sheeran",
+        "follower_count": 180,
+        "description": "Musician",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Gigi Hadid",
+        "follower_count": 75,
+        "description": "Model",
+        "country": "United States"
+    },
+    {
+        "name": "Virender Sehwag",
+        "follower_count": 25,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Roger Federer",
+        "follower_count": 50,
+        "description": "Tennis Player",
+        "country": "Switzerland"
+    },
+    {
+        "name": "Novak Djokovic",
+        "follower_count": 30,
+        "description": "Tennis Player",
+        "country": "Serbia"
+    },
+    {
+        "name": "Conor McGregor",
+        "follower_count": 40,
+        "description": "MMA Fighter",
+        "country": "Ireland"
+    },
+    {
+        "name": "Elon Musk",
+        "follower_count": 160,
+        "description": "Entrepreneur",
+        "country": "United States"
+    },
+    {
+        "name": "Narendra Modi",
+        "follower_count": 70,
+        "description": "Politician",
+        "country": "India"
+    },
+    {
+        "name": "Jeff Bezos",
+        "follower_count": 120,
+        "description": "Entrepreneur",
+        "country": "United States"
+    },
+    {
+        "name": "Mark Zuckerberg",
+        "follower_count": 90,
+        "description": "Entrepreneur",
+        "country": "United States"
+    },
+    {
+        "name": "Cristiano Ronaldo Jr.",
+        "follower_count": 10,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Sachin Tendulkar",
+        "follower_count": 40,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Lewis Hamilton",
+        "follower_count": 80,
+        "description": "Formula 1 Driver",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Michael Jordan",
+        "follower_count": 35,
+        "description": "Basketball Player",
+        "country": "United States"
+    },
+    {
+        "name": "Saweetie",
+        "follower_count": 30,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Simone Biles",
+        "follower_count": 20,
+        "description": "Gymnast",
+        "country": "United States"
+    },
+    {
+        "name": "Anushka Sharma",
+        "follower_count": 50,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "MS Dhoni",
+        "follower_count": 45,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Usain Bolt",
+        "follower_count": 30,
+        "description": "Athlete",
+        "country": "Jamaica"
+    },
+    {
+        "name": "Scarlett Johansson",
+        "follower_count": 65,
+        "description": "Actress",
+        "country": "United States"
+    }
+]
